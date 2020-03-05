0d421f1eee881d51436169bfe9b5b79188061c0e
Hiroyuki Tanaka <ht0919@gmail.com>
2020-03-05 18:24:41 +0900

    Add ch08

---
+++ ch08/test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+try() {
+  ans=`./a.out < $1.txt`
+  if [ "$ans" = "$2" ]; then
+    echo "$1 => $ans"
+  else
+    echo "$1 => $2 expected, but got $ans"
+    exit
+  fi
+}
+
+if [ -e a.out ]; then
+  try test1 "3 4 2 5 1"
+  echo OK
+else
+  echo "a.out not found."
+fi
